fa53d7ae674cb7a90bc80ffb58752e60790d656f
kakeruAoyama <64327383+kakeruAoyama@users.noreply.github.com>
2021-08-07 05:12:59 +0900

    re5-2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
 
   get 'hello/index' => 'hello#index'
   get 'hello/link' => 'hello#link'
-  resources :tweets
+  get 'tweets' => 'tweets#index'
+  get 'tweets/new' => 'tweets#new'
+  post 'tweets' => 'tweets#create'
+  get 'tweets/:id' => 'tweets#show',as: 'tweet'
+  patch 'tweets/:id' => 'tweets#update'
+  delete 'tweets/:id' => 'tweets#destroy'
+  get 'tweets/:id/edit' => 'tweets#edit', as:'edit_tweet'
   root 'hello#index'
 
 end
