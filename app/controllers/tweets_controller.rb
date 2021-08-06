a88c893b7f8ece720f48d8cab4d9c8acb0441379
kakeruAoyama <64327383+kakeruAoyama@users.noreply.github.com>
2021-08-07 07:00:15 +0900

    7-3

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -50,6 +50,6 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:body)
+    params.require(:tweet).permit(:body, :image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_195410) do
+ActiveRecord::Schema.define(version: 2021_08_06_213801) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
