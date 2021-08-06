093d2b0fb487ee646b44facb415279dd5e0e5f8a
kakeruAoyama <64327383+kakeruAoyama@users.noreply.github.com>
2021-08-07 07:17:00 +0900

    7-4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_06_213801) do
+ActiveRecord::Schema.define(version: 2021_08_06_220310) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "tweet_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tweet_id"], name: "index_likes_on_tweet_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
@@ -32,4 +41,6 @@ ActiveRecord::Schema.define(version: 2021_08_06_213801) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "tweets"
+  add_foreign_key "likes", "users"
 end
