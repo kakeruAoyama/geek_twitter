cba671337ebe5319e94d057cd03c153cfed09929
kakeruAoyama <64327383+kakeruAoyama@users.noreply.github.com>
2021-08-06 22:21:23 +0900

    3-13

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'tweets' => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
   post 'tweets' => 'tweets#create'
+  get 'tweets/:id' => 'tweets#show',as: 'tweet'
   root 'hello#index'
 
 end
